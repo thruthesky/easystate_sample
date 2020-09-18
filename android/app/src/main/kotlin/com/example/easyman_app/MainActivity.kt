6a5d0330238d41540ea605f56445d9e75ddc6e0a
JAEHO SONG <thruthesky@gmail.com>
2020-09-19 01:21:21 +0900

    sample app for easystate

---
+++ android/app/src/main/kotlin/com/example/easyman_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.easyman_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import 'package:easystate/easystate.dart';
+
+class CountModel extends EasyState {
+  int _count = 0;
+  dynamic count;
+  CountModel() {
+    count = init<int>(0);
+  }
+  increase() {
+    _count ++;
+    count.add(_count);
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  CountModel countModel = CountModel();
+
+  @override
+  Widget build(BuildContext context) {
+
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Easystate count:',
+            ),
+            StreamBuilder(
+              stream: countModel.count,
+              builder: (context, snapshot) => Text(
+                snapshot.data.toString(),
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => countModel.increase(),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
