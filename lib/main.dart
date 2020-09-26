4fb78fc50b4874a6692a95c3dd31ca401632e7d3
JaeHo Song <thruthesky@gmail.com>
2020-09-26 11:45:38 +0800

    update doc

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:easystate/easystate.dart';
 import 'package:flutter/material.dart';
 
@@ -47,7 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
             EasyBuilder(
               builder: (context, snapshot) => Text(
                 snapshot.data.value.toString(),
-                style: Theme.of(context).textTheme.headline4,
               ),
               model: countModel,
             ),
@@ -56,9 +57,18 @@ class _MyHomePageState extends State<MyHomePage> {
               initialData: countModel.stream,
               builder: (context, snapshot) => Text(
                 snapshot.data.value.toString(),
-                style: Theme.of(context).textTheme.headline4,
               ),
             ),
+            RaisedButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) {
+                    return NewScreen();
+                  },
+                ),
+              ),
+              child: Text('New Screen'),
+            )
           ],
         ),
       ),
@@ -66,6 +76,59 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: () => countModel.increase(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewScreen extends StatefulWidget {
+  const NewScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _NewScreenState createState() => _NewScreenState();
+}
+
+class _NewScreenState extends State<NewScreen> {
+  StreamSubscription stream;
+
+  @override
+  void initState() {
+    stream = countModel.stream.listen((value) => setState(() => null));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    stream.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Saved Suggestions'),
+      ),
+      body: Column(
+        children: [
+          Text(countModel.value.toString()),
+          RaisedButton(
+            onPressed: () => setState(() => countModel.increase()),
+            child: Text('Increase with setState()'),
+          ),
+          Divider(),
+          Text(countModel.value.toString()),
+          RaisedButton(
+            onPressed: () => setState(() => countModel.increase()),
+            child: Text('Increase with stream listing & setState()'),
+          ),
+          RaisedButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Back'),
+          ),
+        ],
       ),
     );
   }
