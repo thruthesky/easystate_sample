20f9d896f715307204f51b345f4268c872edac4f
JaeHo Song <thruthesky@gmail.com>
2020-09-26 11:04:25 +0800

    update doc. add easybuilder

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter/material.dart';
 class CountModel extends EasyState {
   int value = 0;
   increase() {
-    value ++;
+    value++;
     update();
   }
 }
+
 CountModel countModel = CountModel();
 
 void main() {
@@ -30,7 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,11 +44,18 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'EasyState Counter :',
             ),
-            
+            EasyBuilder(
+              builder: (context, snapshot) => Text(
+                snapshot.data.value.toString(),
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              model: countModel,
+            ),
             StreamBuilder(
               stream: countModel.stream,
+              initialData: countModel.stream,
               builder: (context, snapshot) => Text(
-                snapshot.data != null ? snapshot.data.value.toString() : '0',
+                snapshot.data.value.toString(),
                 style: Theme.of(context).textTheme.headline4,
               ),
             ),
